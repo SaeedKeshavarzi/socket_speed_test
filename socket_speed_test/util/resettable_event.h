f4ebc45dd917422a7e1c325b036b967487bae73d
Saeed <Saeed@Keshavarzi.com>
2020-05-12 08:39:27 +0430

    get rid of enable_if in resettable_event

--- socket_speed_test/util/resettable_event.h
+++ socket_speed_test/util/resettable_event.h
@@ -7,115 +7,156 @@
 #include <condition_variable>
 
 template<bool _AutoReset>
-class resettable_event
+class resettable_event;
+
+template<>
+class resettable_event<true> // Auto Reset
 {
 public:
-    static constexpr bool is_auto = _AutoReset;
+	typedef resettable_event<true> type;
+	static constexpr bool is_auto{ true };
 
-    resettable_event(const resettable_event&) = delete;
-    resettable_event& operator=(const resettable_event&) = delete;
+	resettable_event(const resettable_event&) = delete;
+	resettable_event& operator=(const resettable_event&) = delete;
 
-    resettable_event(const bool initial_state = false) :
-        state{ initial_state }
-    { }
+	resettable_event(const bool initial_state = false) :
+		state{ initial_state }
+	{ }
 
-    inline bool is_set() const
-    {
-        return state.load();
-    }
+	inline bool is_set() const
+	{
+		return state.load();
+	}
 
-    inline void set()
-    {
-        std::lock_guard<std::mutex> lock(guard);
+	inline void set()
+	{
+		std::lock_guard<std::mutex> lock(guard);
 
-        if (!state.exchange(true))
-            cv.notify_all();
-    }
+		if (!state.exchange(true))
+			cv.notify_all();
+	}
 
-    inline void reset()
-    {
-        std::lock_guard<std::mutex> lock(guard);
-        state = false;
-    }
+	inline void reset()
+	{
+		std::lock_guard<std::mutex> lock(guard);
+		state = false;
+	}
 
-    template<typename = std::enable_if_t<!is_auto, bool>>
-    inline void wait() const
-    {
-        std::unique_lock<std::mutex> lock(guard);
+	inline void wait()
+	{
+		std::unique_lock<std::mutex> lock(guard);
 
-        while (!state.load())
-            cv.wait(lock);
-    }
+		while (!state.exchange(false))
+			cv.wait(lock);
+	}
 
-    template<class _Rep, class _Period, typename = std::enable_if_t<!is_auto, bool>>
-    inline bool wait_for(const std::chrono::duration<_Rep, _Period> & rel_time)  const
-    {
-        std::unique_lock<std::mutex> lock(guard);
+	template<class _Rep, class _Period>
+	inline bool wait_for(const std::chrono::duration<_Rep, _Period> & rel_time)
+	{
+		std::unique_lock<std::mutex> lock(guard);
 
-        while (!state.load())
-        {
-            if (cv.wait_for(lock, rel_time) == std::cv_status::timeout)
-                return state.load();
-        }
+		while (!state.exchange(false))
+		{
+			if (cv.wait_for(lock, rel_time) == std::cv_status::timeout)
+				return state.exchange(false);
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    template<class _Clock, class _Duration, typename = std::enable_if_t<!is_auto, bool>>
-    inline bool wait_until(const std::chrono::time_point<_Clock, _Duration> & timeout_time)  const
-    {
-        std::unique_lock<std::mutex> lock(guard);
+	template<class _Clock, class _Duration>
+	inline bool wait_until(const std::chrono::time_point<_Clock, _Duration> & timeout_time)
+	{
+		std::unique_lock<std::mutex> lock(guard);
 
-        while (!state.load())
-        {
-            if (cv.wait_until(lock, timeout_time) == std::cv_status::timeout)
-                return state.load();
-        }
+		while (!state.exchange(false))
+		{
+			if (cv.wait_until(lock, timeout_time) == std::cv_status::timeout)
+				return state.exchange(false);
+		}
 
-        return true;
-    }
-
-    template<typename = std::enable_if_t<is_auto, bool>>
-    inline void wait()
-    {
-        std::unique_lock<std::mutex> lock(guard);
-
-        while (!state.exchange(false))
-            cv.wait(lock);
-    }
-
-    template<class _Rep, class _Period, typename = std::enable_if_t<is_auto, bool>>
-    inline bool wait_for(const std::chrono::duration<_Rep, _Period> & rel_time)
-    {
-        std::unique_lock<std::mutex> lock(guard);
-
-        while (!state.exchange(false))
-        {
-            if (cv.wait_for(lock, rel_time) == std::cv_status::timeout)
-                return state.exchange(false);
-        }
-
-        return true;
-    }
-
-    template<class _Clock, class _Duration, typename = std::enable_if_t<is_auto, bool>>
-    inline bool wait_until(const std::chrono::time_point<_Clock, _Duration> & timeout_time)
-    {
-        std::unique_lock<std::mutex> lock(guard);
-
-        while (!state.exchange(false))
-        {
-            if (cv.wait_until(lock, timeout_time) == std::cv_status::timeout)
-                return state.exchange(false);
-        }
-
-        return true;
-    }
+		return true;
+	}
 
 protected:
-    std::atomic_bool state;
-    mutable std::mutex guard;
-    mutable std::condition_variable cv;
+	std::atomic_bool state;
+	mutable std::mutex guard;
+	mutable std::condition_variable cv;
+};
+
+template<>
+class resettable_event<false> // Manual Reset
+{
+public:
+	typedef resettable_event<false> type;
+	static constexpr bool is_auto{ false };
+
+	resettable_event(const resettable_event&) = delete;
+	resettable_event& operator=(const resettable_event&) = delete;
+
+	resettable_event(const bool initial_state = false) :
+		state{ initial_state }
+	{ }
+
+	inline bool is_set() const
+	{
+		return state.load();
+	}
+
+	inline void set()
+	{
+		std::lock_guard<std::mutex> lock(guard);
+
+		if (!state.exchange(true))
+			cv.notify_all();
+	}
+
+	inline void reset()
+	{
+		std::lock_guard<std::mutex> lock(guard);
+		state = false;
+	}
+
+	inline void wait() const
+	{
+		std::unique_lock<std::mutex> lock(guard);
+
+		while (!state.load())
+			cv.wait(lock);
+	}
+
+	template<class _Rep, class _Period>
+	inline bool wait_for(const std::chrono::duration<_Rep, _Period> & rel_time)  const
+	{
+		std::unique_lock<std::mutex> lock(guard);
+
+		while (!state.load())
+		{
+			if (cv.wait_for(lock, rel_time) == std::cv_status::timeout)
+				return state.load();
+		}
+
+		return true;
+	}
+
+	template<class _Clock, class _Duration>
+	inline bool wait_until(const std::chrono::time_point<_Clock, _Duration> & timeout_time)  const
+	{
+		std::unique_lock<std::mutex> lock(guard);
+
+		while (!state.load())
+		{
+			if (cv.wait_until(lock, timeout_time) == std::cv_status::timeout)
+				return state.load();
+		}
+
+		return true;
+	}
+
+protected:
+	std::atomic_bool state;
+	mutable std::mutex guard;
+	mutable std::condition_variable cv;
 };
 
 #endif // !_RESETTABLE_EVENT_H_
